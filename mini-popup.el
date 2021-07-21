3260c80330925be45652fce1050e4394e6ef8a8c
Daniel Mendler <mail@daniel-mendler.de>
2021-07-21 21:44:51 +0200

    Minor cleanup

--- mini-popup.el
+++ mini-popup.el
@@ -98,14 +98,9 @@
 
 (defvar mini-popup--mouse-ignore-map
   (let ((map (make-sparse-keymap)))
-    (dolist (k '(mouse-1 down-mouse-1 drag-mouse-1 double-mouse-1 triple-mouse-1
-                 mouse-2 down-mouse-2 drag-mouse-2 double-mouse-2 triple-mouse-2
-                 mouse-3 down-mouse-3 drag-mouse-3 double-mouse-3 triple-mouse-3
-                 mouse-4 down-mouse-4 drag-mouse-4 double-mouse-4 triple-mouse-4
-                 mouse-5 down-mouse-5 drag-mouse-5 double-mouse-5 triple-mouse-5
-                 mouse-6 down-mouse-6 drag-mouse-6 double-mouse-6 triple-mouse-6
-                 mouse-7 down-mouse-7 drag-mouse-7 double-mouse-7 triple-mouse-7))
-      (define-key map (vector k) #'ignore))
+    (dotimes (i 7)
+      (dolist (k '(mouse down-mouse drag-mouse double-mouse triple-mouse))
+        (define-key map (vector (intern (format "%s-%s" k (1+ i)))) #'ignore)))
     map)
   "Ignore all mouse clicks.")
 
